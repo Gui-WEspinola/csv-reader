3617cd17ca7ff52742ab9843276beb3208c9d637
Guilherme Wanderley Espinola <gwanderleyespinola@gmail.com>
2023-02-09 10:21:26 -0300

    feat: File uploading implemented

--- src/main/java/io/github/guiwespinola/productcsv/controller/ProductController.java
+++ src/main/java/io/github/guiwespinola/productcsv/controller/ProductController.java
@@ -1,6 +1,7 @@
 package io.github.guiwespinola.productcsv.controller;
 
 import com.opencsv.bean.CsvToBeanBuilder;
+import io.github.guiwespinola.productcsv.entity.Product;
 import io.github.guiwespinola.productcsv.entity.dto.ProductDTO;
 import io.github.guiwespinola.productcsv.service.ProductService;
 import lombok.RequiredArgsConstructor;
@@ -10,9 +11,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.FileReader;
+import java.io.InputStreamReader;
 import java.util.List;
 
 @Slf4j
@@ -24,15 +28,30 @@ public class ProductController {
     private final ProductService productService;
 
     @SneakyThrows
-    @PostMapping("/create-product-from-local-file")
-    public ResponseEntity<?> createProduct() {
-        List<ProductDTO> beans = new CsvToBeanBuilder(new FileReader("csv/Products.csv"))
-                .withType(ProductDTO.class)
-                .build()
-                .parse();
+    @PostMapping("/upload")
+    public ResponseEntity<String> createProduct(MultipartFile file) {
+        InputStreamReader reader = new InputStreamReader(file.getInputStream());
+        List<ProductDTO> beans = new CsvToBeanBuilder(reader)
+            .withType(ProductDTO.class)
+            .build()
+            .parse();
+
+        reader.close();
 
         beans.stream().map(productService::save).toList();
 
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+    return ResponseEntity.status(HttpStatus.CREATED).body("File successfully uploaded");
     }
+//    @SneakyThrows
+//    @PostMapping("/create-product-from-local-file")
+//    public ResponseEntity<?> createProduct() {
+//        List<ProductDTO> beans = new CsvToBeanBuilder(new FileReader("csv/Products.csv"))
+//                .withType(ProductDTO.class)
+//                .build()
+//                .parse();
+//
+//        beans.stream().map(productService::save).toList();
+//
+//        return ResponseEntity.status(HttpStatus.CREATED).build();
+//    }
 }
